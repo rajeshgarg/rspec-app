70f300e31d24a45c5ceef2c8cb9220ca0d552f01
RajeshGarg <rajgarg_03@yahoo.co.in>
2013-12-11 14:55:08 +0530

    sample app rspec spork watchr rails3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecApp::Application.config.secret_token = '28be2289e3b70bc07e317714defe0cc3ff9f34e11250e50022724f98de002d8141d428d32aaa3d13a97461cf6d1e3dc7393dfa1fe8ee4c316e5a722a6b067f07'
